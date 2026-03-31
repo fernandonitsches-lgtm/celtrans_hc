b5e56fecb2dd70086149f9011d4bedb6e4df1cb0
Tiago Abdalla <tiagoabdalla2013@gmail.com>
2026-03-31 16:46:03 -0300

    refactor: remove performance polyfills for clearMarks and clearMeasures

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-// Polyfill para evitar erro de clearMarks
-if (!performance.clearMarks) {
-  performance.clearMarks = () => {};
-}
-if (!performance.clearMeasures) {
-  performance.clearMeasures = () => {};
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
